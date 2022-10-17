d6f9b462c1133633e12882d660f3cff4eec2a518
araromi2 <abdulhameedararomi@gmail.com>
2022-10-17 02:49:12 -0700

    Add header file

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char);
+
+#endif /* #ifndef _MAIN_H */
